7390c363425260dd3c6bf5f75c7821c106c921dc
Andrea Franceschini <andrea.franceschini@gmail.com>
2020-05-07 16:42:27 +0100

    This *should* be enough

--- src/GraphSketcher.js
+++ src/GraphSketcher.js
@@ -1,6 +1,26 @@
-import * as GraphView from './GraphView';
-import * as GraphUtils from './GraphUtils';
+import p5 from 'p5';
+import GraphView from './GraphView';
 
-export default {
+const s = (p, _width, _height) => {
+    let width = _width;
+    let height = _height;
 
+    p.setup = () => {
+        p.createCanvas(width, height);
+    }
+
+    p.draw = () => {
+        p.background(255);
+    }
 }
+
+export function makeGraphSketcher(element, width, height) {
+    let sketch;
+    let p = new p5(instance => {
+        sketch = new s(instance, width, height);
+        return sketch;
+    }, element);
+    return { sketch, p };
+}
+
+export * from './GraphView';
