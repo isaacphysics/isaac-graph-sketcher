e9eb850892b67361f6f7beae3e652a03b8c8408d
Andrea Franceschini <andrea.franceschini@gmail.com>
2021-10-08 16:10:01 +0100

    Refactor webpack config and package.json

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+
+module.exports = (_env, argv) => { return {
+  entry: './dist/src/GraphSketcher.js',
+  devtool: false,
+  optimization: {
+    usedExports: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          { loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                "@babel/plugin-transform-typescript",
+                "@babel/plugin-proposal-class-properties",
+                "@babel/plugin-transform-classes"
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          { loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                "@babel/plugin-proposal-class-properties",
+                "@babel/plugin-transform-classes"
+              ]
+            }
+          },
+          { loader: 'ts-loader' }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            },
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname), 'node_modules'],
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      name: 'isaac-graph-sketcher',
+      type: 'umd',
+    }
+  },
+  externals: [
+    /^lodash\/?.*$/,
+    /^p5\/?.*$/
+  ]
+}};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,77 +1,12 @@
 const path = require('path');
 const DashboardPlugin = require('webpack-dashboard/plugin');
+const commonConfig = require('./webpack.config');
+const {merge} = require('webpack-merge');
 
-module.exports = (_env, argv) => { return {
+module.exports = (_env, argv) => { return merge(commonConfig(), {
   entry: './src/GraphSketcher.ts',
-  devtool: argv.mode === 'development' ? 'source-map' : false,
-  optimization: {
-    usedExports: true,
-  },
+  devtool: 'source-map',
   plugins: [
     new DashboardPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          { loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: [
-                "@babel/plugin-transform-typescript",
-                "@babel/plugin-proposal-class-properties",
-                "@babel/plugin-transform-classes"
-              ]
-            }
-          }
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.ts$/,
-        use: [
-          { loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: [
-                "@babel/plugin-proposal-class-properties",
-                "@babel/plugin-transform-classes"
-              ]
-            }
-          },
-          { loader: 'ts-loader' }
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts',
-            },
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    modules: [path.resolve(__dirname), 'node_modules'],
-    extensions: [ '.tsx', '.ts', '.js' ],
-    // alias: {
-    //   'p5': 'p5/lib/p5.min.js'
-    // }
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'isaac-graph-sketcher',
-    libraryTarget: 'umd'
-  },
-  externals: [
-    /^lodash\/?.*$/,
-    /^p5\/?.*$/
   ]
-}};
+})};
